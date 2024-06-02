9a0d91a193b1597380ba027eb3428342fba9b708
jack <otiscurtisdotcom@gmail.com>
2024-06-02 22:51:59 +0100

    Update styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+  const allImageData = [
+    {
+      title: 'Image 1',
+      size: '30cm x 27cm'
+    },
+    {
+      title: 'Lorem ipsum',
+      size: '30cm x 27cm'
+    },
+  ];
+
+  const getImages = () => {
+    return allImageData.map((imageData, i) => {
+      return (
+        <div className='image-block'>
+          <picture>
+            <source
+              media="(max-width: 719px)"
+              srcSet={`${process.env.PUBLIC_URL}/photos/${i}_720.jpg`}
+            />
+            <source
+              media="(min-width: 720px)"
+              srcSet={`${process.env.PUBLIC_URL}/photos/${i}_1440.jpg`}
+            />
+            <img
+              src={`${process.env.PUBLIC_URL}/photos/${i}_1440.jpg`}
+              alt={`Erin Sleeper: ${imageData.title}`}
+            />
+          </picture>
+          <h2>{imageData.title}</h2>
+          <h3>{imageData.size}</h3>
+        </div>
+      )
+    })
+  }
+
   return (
     <section>
       <div className='details'>
-        <p>Erin Sleeper</p>
+        <div className='fixed'>
+          <h1>Erin Sleeper</h1>
+          <p>
+            Email: <a href="mailto:sleeper.e@gmail.com" target='_blank'>sleeper.e@gmail.com</a>
+          </p>
+          <p>
+            Instagram: <a href='https://www.instagram.com/_damozel' target='_blank'>_damozel</a>
+          </p>
+        </div>
       </div>
       <div className='pictures'>
-        <img width="720" src={`${process.env.PUBLIC_URL}/photos/0_1440.jpg`} />
-        <img width="720" src={`${process.env.PUBLIC_URL}/photos/1_1440.jpg`} />
-        <img width="720" src={`${process.env.PUBLIC_URL}/photos/2_1440.jpg`} />
-        <img width="720" src={`${process.env.PUBLIC_URL}/photos/3_1440.jpg`} />
+        {getImages()}
       </div>
     </section>
   );
